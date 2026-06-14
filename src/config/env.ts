03fdb4d3fc51cf310620b64a46bf1c826c5bf5e7
shijas <shijas@example.com>
2026-06-14 20:48:33 +0530

    revert: rollback wallet.ts BSC gas settings and env changes

--- src/config/env.ts
+++ src/config/env.ts
@@ -36,7 +36,7 @@ const envSchema = z.object({
     RELAYER_PRIVATE_KEY: z.string().default(""),
     MASTER_WALLET_SEED: z.string().default(""),
     BASE_RPC_URL: z.string().default("https://sepolia.base.org"),
-    BSC_RPC_URL: z.string().default("https://bsc-dataseed.binance.org"),
+    BSC_RPC_URL: z.string().default("https://bsc-dataseed.binance.org/"),
     USDC_ADDRESS: addressSchema.default("0x036CbD53842c5426634e7929541eC2318f3dCF7e"),
     USDT_ADDRESS: addressSchema.default("0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2"), // Axelar Wrapped USDT
 
--- src/services/wallet.ts
+++ src/services/wallet.ts
@@ -275,11 +275,6 @@ class WalletService {
         const amountUnits = ethers.parseUnits(amountStr, decimals);
 
         const txOptions: any = {};
-        if (chain === 'bsc') {
-            const feeData = await this.getProvider(chain).getFeeData();
-            txOptions.gasPrice = feeData.gasPrice || ethers.parseUnits('3', 'gwei');
-            txOptions.gasLimit = 200000n;
-        }
 
         const tx = await escrowContract.withdraw(tokenAddress, amountUnits, txOptions);
         await tx.wait();
